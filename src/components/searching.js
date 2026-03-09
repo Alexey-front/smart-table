26d2ad61fc897d0fb1680efd29ff76fe4bdb3dab
Alex <aaholodkov@bk.ru>
2026-03-09 18:35:28 +0300

    Первый и точно не последний

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,13 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
+const compare = createComparison(['rules.skipEmptyTargetValues()'], [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)])
+ 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+         if (!action?.target || action.target.search === '' || action.target.search?.trim() === '') {
+        return data;  // Возвращаем все данные без изменений
+    }  
+        else return data.filter(item => compare(item, action.target))
     }
 }
--- src/components/table.js
+++ src/components/table.js
@@ -10,14 +10,38 @@ import {cloneTemplate} from "../lib/utils.js";
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
-
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
-
+    before.reverse().forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.prepend(root[subName].container);
+    });
+    after.forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.append(root[subName].container);
+    })
     // @todo: #1.3 —  обработать события и вызвать onAction()
-
+    root.container.addEventListener('change', event => {
+        onAction();
+    })
+    root.container.addEventListener('reset', event => {
+        setTimeout(onAction)
+    })
+    root.container.addEventListener('submit', event => {
+        event.preventDefault();
+        onAction(event.submitter)
+    })
+    
+    
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+          const row = cloneTemplate(rowTemplate)
+          Object.keys(item).forEach(key => {
+           const currRow = row.elements[key] 
+            if(currRow) currRow.textContent = item[key]
+        })
+        return row.container
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
